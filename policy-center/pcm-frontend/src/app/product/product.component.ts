f2aad326254dbc370f806f669bfa82db4d7c7556
zhzhh <zhihongzhang_hi@163.com>
2018-07-01 16:08:02 +0800

    星级评价组件

--- policy-center/pcm-frontend/src/app/product/product.component.ts
+++ policy-center/pcm-frontend/src/app/product/product.component.ts
@@ -16,11 +16,10 @@ export class ProductComponent implements OnInit {
     this.products = [
         new Product(1,"first product",1.99, 3.1, "first product desc" ,  ["电子产品", "硬件设备"]),
         new Product(2, "second", 2.12, 3.2, "第二个product desc", ["电子产品", "硬件设备"] ),
-        new Product(3, "third", 2.13, 3.3, "第三个 product desc", [ "硬件设备"] ),
-        new Product(4, "forth", 2.14, 3.4, "第四个 product desc", ["电子产品", "硬件设备"] ),
-        new Product(5, "fifth", 2.15, 3.5, "第五个 product desc", ["电子产品", "硬件设备"] ),
+        new Product(3, "third", 2.13, 4.3, "第三个 product desc", [ "硬件设备"] ),
+        new Product(4, "forth", 2.14, 5.4, "第四个 product desc", ["电子产品", "硬件设备"] ),
+        new Product(5, "fifth", 2.15, 6.5, "第五个 product desc", ["电子产品", "硬件设备"] ),
         new Product(6, "sisth", 2.16, 3.6, "第六个 product desc", ["图书"] )
-      
     ]
   }
 
